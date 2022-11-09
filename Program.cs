ff0f005f8d76ef831f11e14611c8729b29a80930
ArtemXa <artemxachik@gmail.com>
2022-11-09 23:05:54 +0300

    V5

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string[] firstArray = new string[5] {"Russian", "Denmrak", ":-)", "123", ":-("};
+string[] secondArray = new string[firstArray.Length];
+void ArrayOfThreeCharact(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+    if(firstArray[i].Length <= 3)
+        {
+        secondArray[count] = firstArray[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArrayOfThreeCharact(firstArray, secondArray);
+PrintArray(secondArray);
